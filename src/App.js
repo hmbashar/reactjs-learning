a0c01902187a294e68814cecde55076d94b5e0ef
hmbashar <hmbashar@gmail.com>
2019-10-14 23:45:28 +0600

    class-03

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
-}
 
+class App extends Component {
+  state = {
+    siteTitle: 'This is a site title',
+    products: [
+      {
+        id:10,
+        name: 'Product Name',
+        desc: 'this is product description'
+      },
+      {
+        id:11,
+        name: 'Product Name 2',
+        desc: 'this is product description'
+      },
+      {
+        id:12,
+        name: 'Product Name 3',
+        desc: 'this is product description'
+      },
+      {
+        id:13,
+        name: 'Product Name 4',
+        desc: 'this is product description'
+      },
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Header/>        
+        <h2>{this.state.siteTitle}</h2>
+        <h3>Products</h3>
+        {
+          this.state.products.map((product) => (
+            <li key={product.id}>ID: {product.id}<br/>Name: {product.name}<br/> Description: {product.desc}</li>
+          ))
+        }
+      </div>
+    )
+  }
+}
 export default App;
