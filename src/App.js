4c824c07980d125706df6eade9ed945aebba9ecb
hmbashar <hmbashar@gmail.com>
2019-10-29 07:45:47 +0600

    class-5 event, onclick, onchange

--- src/App.js
+++ src/App.js
@@ -28,15 +28,34 @@ class App extends Component {
         name: 'Product Name 4',
         desc: 'this is product description'
       },
-    ]
+    ],
+    inputtext: '',
+    showProduct: false,
+  }
+  testHandel = (e) => {
+
+    this.setState({inputtext:e.target.value});
+    console.log(this.state.inputtext);
+  }
+  showProduct = (e) => {
+    this.setState({showProduct:true});
   }
   render() {
+    var showProductMarkup = '';
+    if(this.state.showProduct=== true) {
+      showProductMarkup =  <Products items={this.state.products}/>
+    }
     return (
       <div>
         <Header/>        
         <h2>{this.state.siteTitle}</h2>
-        <h3>Products</h3>
-        <Products items={this.state.products}/>
+        <h3>Products</h3>        
+
+        <form action="">
+          <input type="text" placeholder="Name" onChange={this.testHandel}/>
+        </form>
+        <button onClick={this.showProduct}>Show Product</button>
+        {showProductMarkup}
       </div>
     )
   }
